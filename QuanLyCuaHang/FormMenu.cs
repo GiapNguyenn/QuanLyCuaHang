e7a9011e1c86f2f0172b19c6b17e87c77298c7a2
GiapNguyenn <139568440+GiapNguyenn@users.noreply.github.com>
2024-01-24 22:15:32 +0700

    cập nhật Menu

---
+++ QuanLyCuaHang/FormMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHang
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MatHang_Click(object sender, EventArgs e)
+        {
+            FormMatHang formMatHang = new FormMatHang();
+            formMatHang.ShowDialog();
+        }
+
+        private void HoaDon_Click(object sender, EventArgs e)
+        {
+            FormHoaDon formHoaDon = new FormHoaDon();
+            formHoaDon.ShowDialog();
+        }
+    }
+}
